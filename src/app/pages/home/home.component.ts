39bda1230bd1c376dd5a373e6fb6eb0e47475192
Ahmad Khuzha <121235222+AhmadKhuzha@users.noreply.github.com>
2024-02-23 12:11:36 +0300

    Commit from TeleportHQ

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core'
+import { Title, Meta } from '@angular/platform-browser'
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.component.html',
+  styleUrls: ['home.component.css'],
+})
+export class Home {
+  raw7k6y: string = ' '
+  rawzefb: string = ' '
+  rawuccw: string = ' '
+  raw74uu: string = ' '
+  raw6xyg: string = ' '
+  raw7wt4: string = ' '
+  rawukin: string = ' '
+  raw9ptw: string = ' '
+  rawcjg1: string = ' '
+  raw0h84: string = ' '
+  rawxfhy: string = ' '
+  raw5qsh: string = ' '
+  raw8qgu: string = ' '
+  rawk7zq: string = ' '
+  raw5w09: string = ' '
+  rawfp63: string = ' '
+  rawr2o0: string = ' '
+  rawea8h: string = ' '
+  rawvvi2: string = ' '
+  raw54e9: string = ' '
+  rawrvue: string = ' '
+  constructor(private title: Title, private meta: Meta) {
+    this.title.setTitle('Opulent Ironclad Snake')
+    this.meta.addTags([
+      {
+        property: 'og:title',
+        content: 'Opulent Ironclad Snake',
+      },
+    ])
+  }
+}
